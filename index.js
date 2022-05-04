301658c151e03e5832ce957f3b918cb43a639029
Shamimzz <shamimislam5091@gmail.com>
2022-05-05 00:13:06 +0600

    added

---
+++ index.js
@@ -0,0 +1,18 @@
+// const stu = [
+//     {name: "Shamim", ID: 49, Batch: 'CSE 13th'}
+// ]
+
+
+
+
+// Create and display an array:
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = fruits;
+
+// First sort the array
+fruits.sort();
+
+// Then reverse it:
+fruits.reverse();
+
+document.getElementById("demo2").innerHTML = fruits;
