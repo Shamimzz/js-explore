a2e7ecfb526f655839d8d252f794eb9834b048dc
Shamimzz <shamimislam5091@gmail.com>
2022-05-07 23:38:03 +0600

    added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 
 
+/*
+
 
 // Create and display an array:
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
@@ -16,3 +18,12 @@ fruits.sort();
 fruits.reverse();
 
 document.getElementById("demo2").innerHTML = fruits;
+
+*/
+
+
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+console.log(myChildren);
