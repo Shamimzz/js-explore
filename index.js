29b26146d8f42902516aca2fe6cabde593be3153
Shamimzz <shamimislam5091@gmail.com>
2022-05-07 23:51:07 +0600

    ad

--- index.js
+++ index.js
@@ -22,8 +22,13 @@ document.getElementById("demo2").innerHTML = fruits;
 */
 
 
-const myGirls = ["Cecilie", "Lone"];
-const myBoys = ["Emil", "Tobias", "Linus"];
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
 
-const myChildren = myGirls.concat(myBoys);
-console.log(myChildren);
+// const myChildren = myGirls.concat(myBoys);
+// console.log(myChildren);
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const result = fruits.splice(2, 2, "Lemon", "Kiwi");
+console.log(result);
