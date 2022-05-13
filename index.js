8e8c157c9ae838c035631fd1ad067dbe84c064ca
Shamimzz <shamimislam5091@gmail.com>
2022-05-13 20:12:43 +0600

    added

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-// const stu = [
-//     {name: "Shamim", ID: 49, Batch: 'CSE 13th'}
-// ]
-
-
-
+// const stu = [{name:"Shamim", ID: 49, Batch: 'CSE 13th'}] /
 /*
-
 
 // Create and display an array:
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 document.getElementById("demo1").innerHTML = fruits;
 
-// First sort the array
+//First sort the array
 fruits.sort();
 
-// Then reverse it:
+//Then reverse it:
 fruits.reverse();
 
 document.getElementById("demo2").innerHTML = fruits;
 
 */
-
-
 // const myGirls = ["Cecilie", "Lone"];
 // const myBoys = ["Emil", "Tobias", "Linus"];
 
-// const myChildren = myGirls.concat(myBoys);
-// console.log(myChildren);
+//const myChildren = myGirls.concat(myBoys);
+//console.log(myChildren);
 
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-const result = fruits.splice(2, 2, "Lemon", "Kiwi");
-console.log(result);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const result = fruits.splice(2, 2, "Lemon", "Kiwi");
+// console.log(result);
+
+
+
+// 1. how to get random number between 1 to 6 in ludo Game. 
+const ludo = () => {
+   Math.floor(Math.random() * 6);
+}
+console.log(ludo);
+
+
