62a56d584717c7216c26996bb6fac856d3ab6223
Shamimzz <shamimislam5091@gmail.com>
2022-05-19 00:24:19 +0600

    a

--- problem1.js
+++ problem1.js
@@ -11,13 +11,20 @@
 
 
 
-const cars = ["BMW", "Volvo", "Saab", "Ford"];
-list: {
-  text += cars[0] + "<br>";
-  text += cars[1] + "<br>";
-  break list;
-  text += cars[2] + "<br>";
-  text += cars[3] + "<br>";
-}
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// list: {
+//   text += cars[0] + "<br>";
+//   text += cars[1] + "<br>";
+//   break list;
+//   text += cars[2] + "<br>";
+//   text += cars[3] + "<br>";
+// }
 
 
+// Create a Map
+const fruits = new Map();
+
+// Set Map Values
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
